d1e06c95085d62250e9bd7b21b53944af582bedb
Carolina Pulido Gómez <priyacarito@gmail.com>
2023-09-16 18:35:46 -0500

    Check if the path exists

--- md-links.js
+++ md-links.js
@@ -7,9 +7,8 @@ const constants = fs.constants;
 //   '.md', '.mkd', '.mdwn', '.mdown', '.mdtxt', '.mdtext', '.markdown', '.text',
 // ];
 
-// const fileExists = (filePath) => access(filePath, constants.F_OK)
-//     .then(() => true)
-//     .catch((err) => reject(new Error(err.message)));
+const fileExists = (filePath) => access(filePath, constants.F_OK);
+// .catch((err) => reject(new Error(err.message)));
 
 const mdlinks = (thePath) => {
   return new Promise((resolve, reject) => {
@@ -18,13 +17,9 @@ const mdlinks = (thePath) => {
         reject(new TypeError('The path is invalid'));
       }
       const absolutePath = path.resolve(thePath);
-      resolve(absolutePath);
-      // const exist = fileExists(absolutePath);
-      // if (exist) {
-      //   resolve(absolutePath);
-      // } else {
-      //   reject(new Error('The path doesn\'t exist'));
-      // }
+      fileExists(absolutePath)
+          .then(() => resolve(absolutePath))
+          .catch((err) => reject(new Error('No such file or directory')));
     } catch (err) {
       reject(new Error(err.message));
     }
@@ -32,7 +27,8 @@ const mdlinks = (thePath) => {
 };
 
 // const thePath = 200;
-// const thePath = './some/example.md';
+// const thePath = './somes/example.md';
+// const thePath = './somes/example.md';
 // mdlinks(thePath)
 //     .then((res) => console.log(res))
 //     .catch((err) => console.log(err.message));
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fsPromises = require('fs/promises');
 const {mdlinks} = require('../md-links');
 
 describe('mdLinks', () => {
@@ -6,20 +7,31 @@ describe('mdLinks', () => {
     expect(typeof mdlinks).toBe('function');
   });
 
-  it('should reject with an invalid type of path or empty string', () => {
-    expect(mdlinks()).rejects.toThrow('The path is invalid');
-    expect(mdlinks(null)).rejects.toThrow('The path is invalid');
-    expect(mdlinks(undefined)).rejects.toThrow('The path is invalid');
-    expect(mdlinks('')).rejects.toThrow('The path is invalid');
-    expect(mdlinks(2)).rejects.toThrow('The path is invalid');
-    expect(mdlinks([])).rejects.toThrow('The path is invalid');
-    expect(mdlinks({}, null, undefined)).rejects.toThrow('The path is invalid');
+  it('should reject with an invalid type of path or empty string', async () => {
+    await expect(mdlinks()).rejects.toThrow('The path is invalid');
+    await expect(mdlinks(null)).rejects.toThrow('The path is invalid');
+    await expect(mdlinks(undefined)).rejects.toThrow('The path is invalid');
+    await expect(mdlinks('')).rejects.toThrow('The path is invalid');
+    await expect(mdlinks(2)).rejects.toThrow('The path is invalid');
+    await expect(mdlinks([])).rejects.toThrow('The path is invalid');
+    await expect(mdlinks({}, null, undefined)).rejects.toThrow('The path is invalid');
   });
 
-  it('should resolve with a path into an aboslute path', () => {
+  it('should reject with an abs. path that doesn\'t exist on pc', async () => {
     jest.spyOn(path, 'resolve').mockImplementation((...args) => {
       return '/mocked/absolute/path';
     });
-    expect(mdlinks('/source')).resolves.toStrictEqual('/mocked/absolute/path');
+    const mockResponse = jest.fn().mockRejectedValue({message: 'reject'});
+    jest.spyOn(fsPromises, 'access').mockResolvedValue(mockResponse);
+    await expect(mdlinks('./source')).rejects.toThrow('No such file or directory');
+  });
+
+  it('should resolve with an abs. path that exists on pc', async () => {
+    jest.spyOn(path, 'resolve').mockImplementation((...args) => {
+      return '/mocked/absolute/path';
+    });
+    // const mockResponse = jest.fn().mockResolvedValue({message: 'resolve'});
+    // jest.spyOn(fsPromises, 'access').mockResolvedValue(mockResponse);
+    await expect(mdlinks('./some/example.md')).resolves.toStrictEqual('./some/example.md');
   });
 });
