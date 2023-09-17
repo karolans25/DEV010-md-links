ac46bed986e53b17fe274640b616475c8c5bd9e4
Carolina Pulido Gómez <priyacarito@gmail.com>
2023-09-16 22:26:06 -0500

    get Link from a url

--- md-links.js
+++ md-links.js
@@ -1,13 +1,63 @@
 const fs = require('fs/promises');
 const path = require('path');
+const markdownIt = require('markdown-it');
 
 const { access } = fs;
 const { constants } = fs;
 const { readFile } = fs;
+const md = markdownIt({ linkify: true });
 
 const markDownExtensions = [
   '.md', '.mkd', '.mdwn', '.mdown', '.mdtxt', '.mdtext', '.markdown', '.text',
 ];
+
+const verifyUrl = (url) => fetch(url)
+  .then((res) => {
+    const data = {
+      status: res.status,
+      ok: res.statusText.toLowerCase(),
+    }
+    return data;
+  })
+  .catch((err) => err);
+
+const getLinksFromHtml = (filePath, text, validate) => new Promise((resolve, reject) => {
+  try{
+    const links = [];
+    const html = md.render(text);
+    const lines = html.split('\n');
+    const max = lines.length;
+    for (let i = 0; i < max; i++) {
+      const regex = /<a\s+(?:[^>]*?\s+)?href=(["'])(.*?)\1>(.*?)<\/a>/g;
+      let match;
+      while ((match = regex.exec(lines[i])) !== null) {
+        const link = {
+          href: match[2],
+          text: match[3],
+          file: filePath,
+          line: parseInt(i, 10) + 1,
+        };
+        links.push(link);
+      }
+    }
+    if (validate) {
+      const linksVerified = links.map((link) => verifyUrl(link.href)
+        .then((res) => {
+          link.status = res.status;
+          link.ok = res.ok;
+          return link;
+        }));
+
+      Promise.all(linksVerified).then((result) => {
+        resolve(result);
+      });
+    } else {
+      resolve(links);
+    }
+  } catch (err){
+    reject(new Error (err.message));
+  }
+});
 
 const fileExists = (filePath) => access(filePath, constants.F_OK);
 // .catch((err) => reject(new Error(err.message)));
@@ -16,7 +66,7 @@ const readAFile = (file) => readFile(file, 'utf8')
   .then((markdown) => markdown)
   .catch((err) => new Error(err.message));
 
-const mdlinks = (thePath) => new Promise((resolve, reject) => {
+const mdlinks = (thePath, validate) => new Promise((resolve, reject) => {
   try {
     if (typeof thePath !== 'string' || thePath === '') {
       reject(new TypeError('The path is invalid'));
@@ -35,8 +85,8 @@ const mdlinks = (thePath) => new Promise((resolve, reject) => {
         }
         readAFile(absolutePath)
           .then((text) => {
-            console.log(text);
-            resolve(absolutePath);
+            const links = getLinksFromHtml(absolutePath, text, validate);
+            resolve(links);
           })
           .catch((err) => reject(new Error('Couldn\'t read the file')));
       })
