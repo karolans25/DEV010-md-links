e31ca38e455bf262c57739508a59414258b17699
Carolina Pulido Gómez <priyacarito@gmail.com>
2023-09-16 21:49:56 -0500

    read a fike

--- md-links.js
+++ md-links.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const { access } = fs;
 const { constants } = fs;
+const { readFile } = fs;
 
 const markDownExtensions = [
   '.md', '.mkd', '.mdwn', '.mdown', '.mdtxt', '.mdtext', '.markdown', '.text',
@@ -10,6 +11,10 @@ const markDownExtensions = [
 
 const fileExists = (filePath) => access(filePath, constants.F_OK);
 // .catch((err) => reject(new Error(err.message)));
+
+const readAFile = (file) => readFile(file, 'utf8')
+  .then((markdown) => markdown)
+  .catch((err) => new Error(err.message));
 
 const mdlinks = (thePath) => new Promise((resolve, reject) => {
   try {
@@ -22,13 +27,18 @@ const mdlinks = (thePath) => new Promise((resolve, reject) => {
         const splitPath = absolutePath.split('/');
         const splitExt = splitPath.pop().split('.');
         if (splitExt.length <= 1 || splitExt[splitExt.length - 1] === '') {
-          reject(new Error('Invalid extension'));
+          reject(new Error('It\'s a directory or invalid extension'));
         }
         const ext = `.${splitExt.pop()}`;
         if (!markDownExtensions.includes(ext)) {
           reject(new Error('File is not a markdown file'));
         }
-        resolve(absolutePath);
+        readAFile(absolutePath)
+          .then((text) => {
+            console.log(text);
+            resolve(absolutePath);
+          })
+          .catch((err) => reject(new Error('Couldn\'t read the file')));
       })
       .catch((err) => reject(new Error('No such file or directory')));
   } catch (err) {
@@ -39,10 +49,11 @@ const mdlinks = (thePath) => new Promise((resolve, reject) => {
 // const thePath = 200;
 // const thePath = './somes/example.md';
 // const thePath = './some/example.txt';
+// const thePath = './some/';
 // const thePath = './some/example.md';
 // mdlinks(thePath)
-//     .then((res) => console.log(res))
-//     .catch((err) => console.log(err.message));
+//   .then((res) => console.log(res))
+//   .catch((err) => console.log(err.message));
 
 module.exports = {
   mdlinks,
