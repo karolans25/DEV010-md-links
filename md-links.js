0cfcd15fe4d2593236e01bf8ba12d599056fa175
Carolina Pulido Gómez <priyacarito@gmail.com>
2023-09-23 16:50:54 -0500

    read a dir and get the links of its mdFiles

--- md-links.js
+++ md-links.js
@@ -1,10 +1,11 @@
-const fs = require('fs/promises');
+const fs = require('fs');
 const path = require('path');
 const markdownIt = require('markdown-it');
 
-const { access } = fs;
-const { constants } = fs;
-const { readFile } = fs;
+const { access } = fs.promises;
+const { constants } = fs.promises;
+const { readFile } = fs.promises;
+const { readdirSync } = fs;
 const md = markdownIt({ linkify: true });
 
 const markDownExtensions = [
@@ -25,11 +26,10 @@ const verifyUrl = (url) => fetch(url)
       ok: 'fail',
     };
     return data;
-    console.log(err);
   });
 
 const getLinksFromHtml = (filePath, text, validate) => new Promise((resolve, reject) => {
-  try{
+  try {
     const links = [];
     const html = md.render(text);
     const lines = html.split('\n');
@@ -51,7 +51,8 @@ const getLinksFromHtml = (filePath, text, validate) => new Promise((resolve, rej
       const linksVerified = links.map((link) => verifyUrl(link.href)
         .then((res) => {
           link.status = res.status;
-          link.ok = res.ok;
+          // link.ok = res.ok;
+          link.ok = (res.status === 200) ? res.ok : 'failed';
           return link;
         }));
 
@@ -62,7 +63,7 @@ const getLinksFromHtml = (filePath, text, validate) => new Promise((resolve, rej
       resolve(links);
     }
   } catch (err) {
-    reject(err);
+    reject(new Error(err.message));
   }
 });
 
@@ -73,6 +74,17 @@ const readAFile = (file) => readFile(file, 'utf8')
   .then((markdown) => markdown)
   .catch((err) => new Error(err.message));
 
+const checkExtension = (fileNameArray) => {
+  if (!(fileNameArray.length > 1)) {
+    return false;
+  }
+  const ext = `.${fileNameArray.pop()}`;
+  if (!markDownExtensions.includes(ext)) {
+    return false;
+  }
+  return true;
+};
+
 const mdlinks = (thePath, validate) => new Promise((resolve, reject) => {
   try {
     if (typeof thePath !== 'string' || thePath === '') {
@@ -81,36 +93,79 @@ const mdlinks = (thePath, validate) => new Promise((resolve, reject) => {
     const absolutePath = path.resolve(thePath);
     fileExists(absolutePath)
       .then(() => {
+        let links = [];
         const splitPath = absolutePath.split('/');
         const splitExt = splitPath.pop().split('.');
-        if (splitExt.length <= 1) {
-          reject(new Error('It\'s a directory'));
+        if (splitExt.length <= 1) { // It's a directory
+          const files = readdirSync(absolutePath);
+          const mdFiles = [];
+          files.forEach((file) => {
+            const fileNameArray = file.split('.');
+            const isMd = checkExtension(fileNameArray);
+            if (isMd) {
+              const joinedRoute = path.join(absolutePath, file);
+              mdFiles.push(joinedRoute);
+            }
+          });
+          links = mdFiles.map((route) => readAFile(route)
+            .then((text) => {
+              links = getLinksFromHtml(absolutePath, text, validate);
+              return links;
+            })
+            .catch((err) => reject(err)));
+          Promise.all(links).then((result) => {
+            resolve(result.flat());
+          });
+        } else { // It's a file
+          // const ext = `.${splitExt.pop()}`;
+          const isMd = checkExtension(splitExt);
+          // if (splitExt[splitExt.length - 1] === '' && !markDownExtensions.includes(ext)) {
+          if (!isMd) {
+            reject(new Error('File extension is not a markdown type'));
+          }
+          readAFile(absolutePath)
+            .then((text) => {
+              links = getLinksFromHtml(absolutePath, text, validate);
+              resolve(links);
+            })
+            // .catch((err) => reject(new Error('Couldn\'t read the file')));
+            .catch((err) => reject(err));
         }
-        const ext = `.${splitExt.pop()}`;
-        if (splitExt[splitExt.length - 1] === '' && !markDownExtensions.includes(ext)) {
-          reject(new Error('Path is not a markdown file'));
-        }
-        readAFile(absolutePath)
-          .then((text) => {
-            const links = getLinksFromHtml(absolutePath, text, validate);
-            resolve(links);
-          })
-          // .catch((err) => reject(new Error('Couldn\'t read the file')));
-          .catch((err) => reject(err));
+
+        // LS DIR
+        // const fs = require('fs');
+
+        // const directoryPath = './path/to/directory';
+        // const files = fs.readdirSync(directoryPath);
+
+        // files.forEach((file) => {
+        //   console.log(file);
+        // });
+
+        // JOIN ROUTES
+        // const path = require('path');
+
+        // const route1 = '/users';
+        // const route2 = '/profile';
+
+        // const joinedRoute = path.join(route1, route2);
+
+        // console.log(joinedRoute);
       })
       // .catch((err) => reject(new Error('No such file or directory')));
-      .catch((err) => reject(err));
+      .catch((err) => reject(new Error(err.message)));
   } catch (err) {
     reject(new Error(err.message));
   }
 });
 
 // const thePath = 200;
+// const thePath = null;
 // const thePath = './somes/example.md';
 // const thePath = './some/example.txt';
-// const thePath = './some/';
 // const thePath = './some/example.md';
-const thePath = './some/example1.md';
+// const thePath = './some/example1.md';
+const thePath = './some/';
 mdlinks(thePath, true)
   .then((res) => console.log(res))
   .catch((err) => console.log(err.message));
